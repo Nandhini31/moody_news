a7166d5dab349c7c9e7a3c4f77d7c96c0d5f7458
nandhini <nandhini31@gmail.com>
2017-08-30 11:12:06 +0100

    happy working

--- src/APICall.js
+++ src/APICall.js
@@ -5,13 +5,13 @@
   APICall.prototype.getHappyResponse = function() {
     var request = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/technology/2014/feb/18/doge-such-questions-very-answered?show-fields=body";
     var self = this;
+    var temp = ""
     $.get(request, function (response){
       responseNewsBody = response.response.content.fields.body;
       console.log(responseNewsBody);
       self.newsBody = responseNewsBody;
+      exports.document.getElementById('app').innerHTML = self.newsBody;
     });
-    exports.document.getElementById('app').innerHTML = this.newsBody;
-    return this.newsBody;
   };
 exports.APICall = APICall;
 })(this);
