b2e60320819bb471d0655f9bff99682f6d275e97
Ryan Davidson <daviryan@aol.com>
2017-08-31 10:07:15 +0100

    Add express

--- index.js
+++
@@ -1,7 +0,0 @@
-var httpServer = require("http-server");
-var path = require("path");
-
-var pathToHtmlAndJsFiles = path.join(__dirname);
-var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
-server.listen(3000);
-console.log("Server is running on port 3000");
