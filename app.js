50a2d8491f782dc1bb2100febfd52e6c4155e319
Sapphire Mason-Brown <saphmb@gmail.com>
2017-08-31 11:12:26 +0100

    Node setup continued

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+
+
+var AylienNewsApi = require('aylien-news-api');
+module.exports.AylienNewsApi = AylienNewsApi
+
+require('./public/javascripts/test')
+// var apiInstance = new AylienNewsApi.DefaultApi();
+
+// // Configure API key authorization: app_id
+// var app_id = apiInstance.apiClient.authentications['app_id'];
+// app_id.apiKey = process.env.X_AYLIEN_NewsAPI_Application_ID;
+//
+// // Configure API key authorization: app_key
+// var app_key = apiInstance.apiClient.authentications['app_key'];
+// app_key.apiKey = process.env.X_AYLIEN_NewsAPI_Application_Key;
+//
+// var opts = {
+//   'title': 'trump',
+//   'sortBy': 'social_shares_count.facebook',
+//   'language': ['en'],
+//   'notLanguage': ['es', 'it'],
+//   'publishedAtStart': 'NOW-7DAYS',
+//   'publishedAtEnd': 'NOW',
+//   'entitiesBodyLinksDbpedia': [
+//     'http://dbpedia.org/resource/Donald_Trump',
+//     'http://dbpedia.org/resource/Hillary_Rodham_Clinton'
+//   ]
+// };
+//
+// var callback = function(error, data, response) {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('API called successfully. Returned data: ');
+//     console.log('========================================');
+//     for (var i = 0; i < data.stories.length; i++){
+//       console.log(data.stories[i].title + " / " + data.stories[i].source.name);
+//     }
+//   }
+// };
+// apiInstance.listStories(opts, callback);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
